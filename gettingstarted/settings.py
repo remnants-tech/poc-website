49f2890dd0237733a445f937136a3d614cdd5e9d
jaewonKwon <34426615+jaewonKwon@users.noreply.github.com>
2017-12-16 16:30:40 -0500

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'christ_connects_app'
+    'app'
 )
 
 MIDDLEWARE_CLASSES = (
