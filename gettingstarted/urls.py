0b5d90a9807d078a5fa15327d9e7d066abef6263
jaewonKwon <34426615+jaewonKwon@users.noreply.github.com>
2017-12-16 16:31:20 -0500

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import christ_connects_app.views
+import app.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
